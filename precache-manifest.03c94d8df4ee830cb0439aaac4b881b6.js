45e685b40856c73fdad0d850ace8428886866e4f
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-20 17:15:16 +0530

    Updates

---
+++ precache-manifest.03c94d8df4ee830cb0439aaac4b881b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8057c30960405eeafd7b94fb3cdc0a2e",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "f8a42bd2d6ea2690c67f",
+    "url": "/react-quiz/static/js/2.2418c81b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.2418c81b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e78ab2785d5287a4707",
+    "url": "/react-quiz/static/js/main.d9235672.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/react-quiz/static/media/nattu-adnan-unsplash.c11f97a3.jpg"
+  }
+]);
